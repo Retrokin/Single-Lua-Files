95da1724b090c841934e7306c2cb4c862ef80f2c
Retrokin <kavenger45@gmail.com>
2020-02-13 10:54:43 -0700

    Class Update

--- class.lua
+++ class.lua
@@ -6,18 +6,18 @@ function class:new(...)
 	setmetatable(nc,self)
 
 	--run inherits inits; and go through nested tables
-	local function _inheritInit(self,...)
+	local function _inheritInit(self)
 		local inherit=rawget(self,"__inherit")
 		if inherit then
 			for i=#inherit,1,-1 do
 			    local l=inherit[i]
-				_inheritInit(l,...)
+				_inheritInit(l)
 				local func=l.__init
-				if func then func(nc,...) end
+				if func then func(nc) end
 			end
 		end
 	end
-	_inheritInit(self,...)
+	_inheritInit(self)
 
 	if self.__init then self.__init(nc,...) end
 	return nc
@@ -55,17 +55,18 @@ end
 
 function class:typeOf(name,skip_inherit)
 	if self==name or (self.type and self:type()==name) then return true end
-	
+
 	--check inheritances
-	if not skip_inherit and rawget(self,"__inherit") then
-		for i,ltbl in ipairs(self.__inherit) do
+	local inh=rawget(self,"__inherit")
+	if not skip_inherit and inh then
+		for i,ltbl in ipairs(inh) do
 			if ltbl==name or (ltbl.type and ltbl:type()==name) then return true end
 		end
 	end
 	
 	--check metatable
 	local mt=getmetatable(self)	
-	if mt then return class.typeOf(self,name,skip_inherit) end
+	if mt then return class.typeOf(mt,name,skip_inherit) end
 
 	return false
 end
